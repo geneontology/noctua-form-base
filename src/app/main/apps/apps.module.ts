bc15810181a4425ad5bb8147c7fc3b5a78522221
tmushayahama <tmushayahama@gmail.com>
2025-03-19 23:43:16 -0700

    removed unused components

--- src/app/main/apps/apps.module.ts
+++ src/app/main/apps/apps.module.ts
@@ -3,8 +3,6 @@ import { NoctuaSharedModule } from '@noctua/shared.module';
 import { NoctuaFormModule } from './noctua-form/noctua-form.module';
 import { NoctuaGraphModule } from './noctua-graph/noctua-graph.module';
 import { NoctuaSearchModule } from './noctua-search/noctua-search.module';
-import { NoctuaDoctorModule } from './noctua-doctor/noctua-doctor.module';
-import { NoctuaTutorialModule } from './noctua-tutorial/noctua-tutorial.module';
 import { NoctuaPathwayModule } from './noctua-pathway/noctua-pathway.module';
 import { NoctuaAnnotationsModule } from './noctua-annotations/noctua-annotations.module';
 
@@ -15,8 +13,6 @@ import { NoctuaAnnotationsModule } from './noctua-annotations/noctua-annotations
     NoctuaSearchModule,
     NoctuaGraphModule,
     NoctuaAnnotationsModule,
-    NoctuaDoctorModule,
-    NoctuaTutorialModule,
     NoctuaPathwayModule
   ],
   exports: [
@@ -25,8 +21,6 @@ import { NoctuaAnnotationsModule } from './noctua-annotations/noctua-annotations
     NoctuaSearchModule,
     NoctuaAnnotationsModule,
     NoctuaGraphModule,
-    NoctuaDoctorModule,
-    NoctuaTutorialModule,
     NoctuaPathwayModule
   ],
   providers: [
